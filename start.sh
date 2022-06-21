84985753b4f7e53cc5ba47128f2c10a082e0bab5
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-06-21 11:50:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/akashmuraliNEW/Ajax-Extra-Features
+  git clone https://github.com/Aadhi000/Ajax.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
