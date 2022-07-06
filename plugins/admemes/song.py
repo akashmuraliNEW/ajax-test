47713b584a9083abb168cc435fab511a0d3aa66d
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-07-06 19:21:07 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**\n𝕄𝔸𝔻𝔼 𝔹𝕐 ›› [❤️](tg://settings)**\n**ℙ𝕆𝕎𝔼ℝ𝔼𝔻 𝔹𝕐 ›› [ℝ𝕖𝕓𝕖𝕜𝕒𝕙](https://t.me/MM_Rebekah_bot)**'
+        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>**\n𝕄𝔸𝔻𝔼 𝔹𝕐 ›› [❤️](tg://settings)**\n**ℙ𝕆𝕎𝔼ℝ𝔼𝔻 𝔹𝕐 ›› [ℝ𝕖𝕓𝕖𝕜𝕒𝕙](https://t.me/MM_Rebekah_bot)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
