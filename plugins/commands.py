9e821f812e8c5ed7ea4130053265680ee3b5ce95
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2022-07-17 12:40:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -233,6 +233,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('MOVIES', url='https://t.me/PrimeFlixMovies') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
